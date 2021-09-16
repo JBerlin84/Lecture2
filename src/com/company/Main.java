0afcff1d404284a9ac4f134b92a74774411a956d
Jimmy <berlin.jimmy@gmail.com>
2021-09-16 13:55:57 +0200

    master change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("22");
 
+        System.out.println("master change");
+
         System.out.println("felaktigt kommando");
     }
 }
